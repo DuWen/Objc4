6817a89fda72b43af7f7d7b31d8dc33e08906f71
杜文(18053889) <duwen16@gmail.com>
2020-03-24 00:25:49 +0800

    Add VVVTest
    add Comand Line Tool
    Enable Hardened Runtime Set NO

---
+++ objc4-779.1/VVVTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VVVTest
+//
+//  Created by Duwen on 2020/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSObject *obj = [[NSObject alloc] init];
+    }
+    return 0;
+}
